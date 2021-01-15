3c236664c6330d0e40e66aab38bcda6e0638febc
Adrodoc <adrodoc55@googlemail.com>
2021-01-15 16:45:36 +0100

    Remove static assert on integral type from compare_and_swap as some non integral types can be represented by one of the MPI integer types

--- bcl/backends/mpi/atomics.hpp
+++ bcl/backends/mpi/atomics.hpp
@@ -9,7 +9,7 @@ extern MPI_Win win;
 
 template <typename T>
 inline T compare_and_swap(BCL::GlobalPtr<T> ptr, T old_val, T new_val) {
-  static_assert(std::is_integral<T>::value, "BCL::compare_and_swap(): only integral types are supported");
+  // static_assert(std::is_integral<T>::value, "BCL::compare_and_swap(): only integral types are supported");
   T result;
   MPI_Datatype type = get_mpi_type<T>();
   int error_code = MPI_Compare_and_swap(&new_val, &old_val, &result,
